d5780c3d415440a9fc4d618921733b000413a7a8
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-04-12 13:29:01 +0300

    add dispatch actions in components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,16 @@
-
-import { nanoid } from 'nanoid'
 import './App.css'
 import ContactList from './ContactList/ContactList'
 import ContactForm from './ContactForm/ContactForm'
 import SearchBox from './SearchBox/SearchBox'
-import {useSelector,useDispatch} from 'react-redux'
-import { addContact, deleteContact, selectContacts } from '../redux/contactsSlice'
-import {changeFilter,selectNameFilter} from '../redux/filtersSlice'
 
 
 function App() { 
- 
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts)
-  const filters =useSelector(selectNameFilter)
-
-
-
-  const onChangeFilter = (event) => {
-    dispatch(changeFilter(event.target.value))
-  }
-
-
-  const addNewContact = (newContact) => {
-    const finalContact = { ...newContact, id: nanoid() };
-    dispatch(addContact(finalContact))
-  };
-
-  const deletePhoneNumber = (contactId) => {
-    dispatch(deleteContact(contactId))
-  }
-
-  const filteredContacts = contacts.filter(
-    (contact) => contact.name.toLowerCase().includes(filters .toLowerCase()));
-  
   return (
   <div>
   <h1>Phonebook</h1>
-  <ContactForm addNewContact={addNewContact} />
-  <SearchBox filter={filters} onChangeFilter={onChangeFilter} />
-  <ContactList contacts={filteredContacts} deletePhoneNumber={deletePhoneNumber} />
+  <ContactForm />
+  <SearchBox />
+  <ContactList />
 </div>
   )
 }
